02dcfcc2619e6b63e3b5e3c0b9fbd2e745757e64
Hovhannes Mirzoyan <hovhannes.mirzoyan01@gmail.com>
2022-06-20 06:12:29 -0800

    Hovhannes Singleton Task

---
+++ Hovhannes Mirzoyan/Singleton/Government.java
@@ -0,0 +1,46 @@
+package singltone;
+
+import java.util.Objects;
+
+public class Government {
+
+    private static Government government;
+
+    private PrimeMinister primeMinister;
+
+    private String decision;
+
+    private Government() {
+        this.decision = "increase workers salary";
+    }
+
+    public static Government getGovernment() {
+        synchronized (Government.class) {
+            if (Objects.isNull(government)) {
+                government = new Government();
+            }
+            return government;
+        }
+    }
+
+    public void setDecision(String decision) {
+        this.decision = decision;
+    }
+
+    public String getDecision() {
+        return this.decision;
+    }
+
+    public void governmentMeeting() {
+        System.out.println("At the government meeting primeMinister will accept or deny this decision.");
+        primeMinister.doDecision(decision);
+    }
+
+    public PrimeMinister getPresident() {
+        return primeMinister;
+    }
+
+    public void setPresident(PrimeMinister primeMinister) {
+        this.primeMinister = primeMinister;
+    }
+}
---
+++ Hovhannes Mirzoyan/Singleton/Main.java
@@ -0,0 +1,24 @@
+package singltone;
+
+public class Main {
+    public static void main(String[] args) {
+        PrimeMinister primeMinister = PrimeMinister.getPresident();
+
+        primeMinister.setName("Joe");
+        primeMinister.setSurname("Biden");
+        primeMinister.setCountry("USA");
+
+        Government government = Government.getGovernment();
+        government.setPresident(primeMinister);
+
+        government.governmentMeeting();
+
+        Government government1 = Government.getGovernment();
+        government1.setDecision("reduce taxes");
+        government1.governmentMeeting();
+
+        System.out.println(government.getPresident().getName() + "\n"
+                            + government1.getPresident().getName());
+
+    }
+}
