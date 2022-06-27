08f597cb9db27b607da4b4b5a329d5c800d878e6
Ruzanna94 <33309355+Ruzanna94@users.noreply.github.com>
2022-06-28 00:08:53 +0400

    Static Factory

---
+++ Ruzanna.Hovsepyan/Static Factory/Test.java
@@ -0,0 +1,6 @@
+public class Test {
+    public static void main(String[] args) {
+        MultilingualValue name = MultilingualValue.createMultilingualValueFromString("Anna", 7);
+        System.out.println(name);
+    }
+}
